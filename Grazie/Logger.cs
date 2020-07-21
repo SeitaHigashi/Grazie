eb5dda37a3723bf44174cdd1f55e5f165fa598e2
SeitaHigashi <higashi110902@gmail.com>
2020-07-21 09:08:14 +0900

    create evaluationBuffer

--- Grazie/Logger.cs
+++ Grazie/Logger.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Timers;
-using ClosedXML.Excel;
 
 namespace Grazie
 {
     class Logger
     {
         private string docName;
+        private Dictionary<Evaluation, int> evaluationBuffer;
         private Timer timer;
 
         public Logger(string docName)
         {
             this.docName = docName;
+
+            evaluationBuffer = new Dictionary<Evaluation, int>();
+            InitEvaluationBuffer();
 
             timer = new System.Timers.Timer();
             timer.Interval = 10000;
@@ -26,6 +27,19 @@ namespace Grazie
         public void Update()
         {
 
+        }
+
+        private void InitEvaluationBuffer()
+        {
+            evaluationBuffer.Clear();
+            evaluationBuffer.Add(Evaluation.SATISFACTION, 0);
+            evaluationBuffer.Add(Evaluation.GOOD, 0);
+            evaluationBuffer.Add(Evaluation.GOODLUCK, 0);
+        }
+
+        private void AddEvaluation(Evaluation evaluation)
+        {
+            evaluationBuffer[evaluation] += 1;
         }
 
         private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
