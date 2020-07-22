d13a6930fcc5879e982eac88cc4a8c17a41d0192
skytomo <tomo1kawa.fullerene@gmail.com>
2020-07-22 11:42:44 +0900

    Cosmetic change ✨

--- Grazie/Evaluation.cs
+++ Grazie/Evaluation.cs
@@ -1,9 +1,21 @@
 ﻿namespace Grazie
 {
+    /// <summary>
+    /// 評価を表します．
+    /// </summary>
     enum Evaluation
     {
+        /// <summary>
+        /// 満足
+        /// </summary>
         SATISFACTION,
+        /// <summary>
+        /// 普通
+        /// </summary>
         GOOD,
+        /// <summary>
+        /// 頑張って
+        /// </summary>
         GOODLUCK
     }
 }
--- Grazie/Grazie.cs
+++ Grazie/Grazie.cs
@@ -5,58 +5,58 @@ namespace Grazie
 {
     public partial class Grazie : Form
     {
-        private Boolean fullscreen;
+        private bool Fullscreen { get; set; }
 
-        private Logger logger;
+        private Logger Logger { get; set; }
 
         public Grazie()
         {
             InitializeComponent();
 
-            this.fullscreen = false;
+            Fullscreen = false;
 
-            logger = new Logger("log.xlsx");
+            Logger = new Logger("log.xlsx");
 
-            this.KeyDown += Form_KeyDown;
+            KeyDown += Form_KeyDown;
         }
 
         private void Form_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyValue == 122)
             {
-                if (fullscreen)
+                if (Fullscreen)
                 {
-                    this.WindowState = FormWindowState.Normal;
-                    this.FormBorderStyle = FormBorderStyle.Sizable;
-                    fullscreen = false;
+                    WindowState = FormWindowState.Normal;
+                    FormBorderStyle = FormBorderStyle.Sizable;
+                    Fullscreen = false;
                 }
                 else
                 {
-                    this.FormBorderStyle = FormBorderStyle.None;
-                    this.WindowState = FormWindowState.Maximized;
-                    fullscreen = true;
+                    FormBorderStyle = FormBorderStyle.None;
+                    WindowState = FormWindowState.Maximized;
+                    Fullscreen = true;
                 }
             }
         }
 
         private void satisfactionButton_Click(object sender, EventArgs e)
         {
-            logger.AddEvaluation(Evaluation.SATISFACTION);
+            Logger.AddEvaluation(Evaluation.SATISFACTION);
         }
 
         private void goodButton_Click(object sender, EventArgs e)
         {
-            logger.AddEvaluation(Evaluation.GOOD);
+            Logger.AddEvaluation(Evaluation.GOOD);
         }
 
         private void goodluckButton_Click(object sender, EventArgs e)
         {
-            logger.AddEvaluation(Evaluation.GOODLUCK);
+            Logger.AddEvaluation(Evaluation.GOODLUCK);
         }
 
         private void Grazie_FormClosing(object sender, FormClosingEventArgs e)
         {
-            logger.Update();
+            Logger.Update();
         }
     }
 }
